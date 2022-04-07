ae751baf7f535eddae7c108b0480a1989c917129
kadmy95@gmail.com <kadmy95@gmail.com>
2022-04-07 11:57:43 +0300

    Updates

---
+++ precache-manifest.03428fd3de03d74044fdb65898dc08b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8374a1309c870d1897fb",
+    "url": "/react-L1/static/css/main.c00f300e.chunk.css"
+  },
+  {
+    "revision": "8374a1309c870d1897fb",
+    "url": "/react-L1/static/js/main.8374a130.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-L1/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5bf36947e1b0adc5df52",
+    "url": "/react-L1/static/js/2.5bf36947.chunk.js"
+  },
+  {
+    "revision": "eee65c1753b582adcc1f",
+    "url": "/react-L1/static/js/runtime~main.eee65c17.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-L1/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "abf490e686b899311f936551fc2fd8e3",
+    "url": "/react-L1/index.html"
+  }
+];
