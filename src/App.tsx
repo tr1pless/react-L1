f20a03a2f04ef57b3c417db4c538c16c8e2a71dc
kadmy95@gmail.com <kadmy95@gmail.com>
2022-04-28 11:46:38 +0300

    lesson-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,99 @@
-import React, { FC, useEffect, useState, useCallback } from "react";
-import { nanoid } from "nanoid";
+import React, { FC, useState, useMemo } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import "./App.scss";
-import { Form } from "./components/Form";
-import { MessageList } from "./components/MessageList";
-import { AUTHOR } from "./constants";
-import { ChatList } from "./components/Chats/ChatList";
+import './App.scss';
+import { Header } from './components/Header';
+import { Chats } from './pages/Chats';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+import { ChatList } from './components/ChatList';
+import { AUTHOR } from './constants';
+import { nanoid } from 'nanoid';
 
-interface Message {
+export interface Chat {
+  id: string;
+  name: string;
+}
+
+const initialMessage: Messages = {
+  default: [
+    {
+      id: '1',
+      author: AUTHOR.USER,
+      value: 'Hello geekbrains',
+    },
+  ],
+};
+
+export interface Message {
   id: string;
   author: string;
   value: string;
 }
 
+export interface Messages {
+  [key: string]: Message[];
+}
+
 export const App: FC = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
-
-  useEffect(() => {
-    if (
-      messages.length > 0 &&
-      messages[messages.length - 1].author !== AUTHOR.BOT
-    ) {
-      const timeout = setTimeout(() => {
-        setMessages([
-          ...messages,
-          {
-            id: nanoid(),
-            author: AUTHOR.BOT,
-            value: "Please wait for human's answer",
-          },
-        ]);
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeout);
-      };
-    }
-  }, [messages]);
-
-  const addMessage = useCallback((value: string, name: string) => {
-    setMessages((prevMessage) => [
-      ...prevMessage,
-      {
+  const [messages, setMessages] = useState<Messages>(initialMessage);
+  
+  const chatList = useMemo(
+    () =>
+      Object.entries(messages).map((chat) => ({
         id: nanoid(),
-        author: name,
-        value,
-      },
-    ]);
-  }, []);
+        name: chat[0],
+      })),
+    [Object.entries(messages).length]
+  );
+
+  
+  const onAddChat = (chat: Chat) => {
+    if(!messages[chat.name]) {
+      setMessages({
+        ...messages,
+        [chat.name]: [],
+      });
+    }
+  };
+    const onDeleteChat = (chatName: string) => {
+      const newMessages = {...messages};
+      delete newMessages[chatName];
+
+      setMessages({
+        ...newMessages
+      })
+    }
 
   return (
-    <>
-      <div className="wrapper">
-        <div className="chats">
-          <h2>Your Chats</h2>
-          <ChatList />
-        </div>
-        <div className="formWrapper">
-        <MessageList messages={messages} />
-        <Form addMessage={addMessage} />
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+
+          <Route path="chats">
+            <Route
+              index
+              element={<ChatList chatList={chatList} onAddChat={onAddChat} onDeleteChat={onDeleteChat}/>}
+            />
+            <Route
+              path=":chatId"
+              element={
+                <Chats
+                  messages={messages}
+                  setMessages={setMessages}
+                  chatList={chatList}
+                  onAddChat={onAddChat}
+                  onDeleteChat={onDeleteChat}
+                />
+              }
+            />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<h2>404</h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
